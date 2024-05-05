f90f51079e053717215ef35a02357db4430eb6e8
falachabt <bennytenezeu@gmail.com>
2024-05-05 19:15:55 +0100

    maj

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 
 import { StatusBar } from "expo-status-bar";
 import {  StyleSheet, Text, View } from "react-native"
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import "react-native-gesture-handler";
-import Auto from "./Components/Auto";
+// import "react-native-gesture-handler";
 import Command from "./Components/Command";
-import Setting from "./Components/Setting";
 import { colors } from "./utils/colors";
 import HeaderBar from "./Components/HeaderBar";
 
-import { useEffect } from "react";
+
 import { GameProvider } from "./contexts/GameContext";
 
 
